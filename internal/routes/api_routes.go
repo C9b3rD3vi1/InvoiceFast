79adb7f6aebaa987f3383b35626db4ac5205917f
C9b3rD3vi1 <nicksonwe@gmail.com>
2026-04-11 08:26:55 +0300

    Serve HTML from views/ not static/

--- internal/routes/api_routes.go
+++ internal/routes/api_routes.go
@@ -4,9 +4,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// StaticRoutes serves the decoupled frontend pages
+// StaticRoutes serves the decoupled frontend pages from views/ directory
 func StaticRoutes(app *fiber.App) fiber.Router {
-	// Pages should already be handled by the Static middleware
-	// but we need at least one route to make this function work
+	// Serve dashboard and SPA pages from views directory
+	app.Get("/dashboard", func(c *fiber.Ctx) error {
+		return c.SendFile("./views/dashboard/index.html")
+	})
+	app.Get("/invoices", func(c *fiber.Ctx) error {
+		return c.SendFile("./views/invoices/index.html")
+	})
+
 	return app.Group("")
 }
